350df2e04cbdcd4767e8c33fcccdaf0eba623f19
trident2710 <andriydychka@gmail.ocm>
2026-04-01 23:10:48 +0300

    fix build

--- server/cli-runner/src/main/java/ua/kpi/softeng_course/tictactoe/Main.java
+++ server/cli-runner/src/main/java/ua/kpi/softeng_course/tictactoe/Main.java
@@ -1,12 +1,14 @@
 package ua.kpi.softeng_course.tictactoe;
 
+import ua.kpi.softeng_course.tictactoe.model.RoundIdGenerator;
 import ua.kpi.softeng_course.tictactoe.model.TicTacToeRoundImpl;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        var roundModel = new TicTacToeRoundImpl();
+        var roundIdGenerator = new RoundIdGenerator();
+        var roundModel = new TicTacToeRoundImpl(roundIdGenerator);
 
         System.out.println(roundModel.nextAction().orElseThrow().getAction());
     }
